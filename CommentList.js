2d0fa3e70db9c1ad82bd769b15bcea1dca473fbb
philburney <99242947+philburney@users.noreply.github.com>
2022-09-02 13:55:49 +0100

    This is steps 1 to 10.

---
+++ CommentList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Comment from './Comment';
+
+export default function CommentList({ comments }) {
+  if (!comments) {
+    return null;
+  }
+  
+  return (
+    <ul className='comments-list'>
+    { 
+      comments.map( individualComment =>        
+          <Comment comment={individualComment} />
+      )
+    }
+
+    </ul>
+  );
+}
---
+++ commentSlice.js
@@ -0,0 +1,48 @@
+// Import createAsyncThunk and createSlice here.
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+// Create loadCommentsForArticleId here.
+
+export const loadCommentsForArticleId = createAsyncThunk(
+   "comments/loadCommentsForArticleId", 
+   async(id) => {
+     const data = await fetch(`api/articles/${id}/comments`);
+     const json = await data.json();
+     return json;
+   })
+
+// Create postCommentForArticleId here.
+
+export const commentsSlice = createSlice({
+  name: 'comments',
+  initialState: {
+    // Add initial state properties here.
+    byArticleId: {},
+    isLoadingComments: false,
+    failedToLoadComments: false
+
+  },
+ extraReducers: (builder) => {
+    builder
+      .addCase(loadCommentsForArticleId.pending, (state) => {
+        state.isLoadingComments = true;
+        state.failedToLoadComments = false;
+      })
+      .addCase(loadCommentsForArticleId.fulfilled, (state, action) => {
+        state.isLoadingComments = false;
+        state.failedToLoadComments = false;
+        state.byArticleId = {
+             [action.payload.articleId] : action.payload.comments
+        };
+      })
+      .addCase(loadCommentsForArticleId.rejected, (state, action) => {
+        state.isLoadingComments = false;
+        state.failedToLoadComments = true;
+      })
+  },
+});
+
+export const selectComments = (state) => state.comments.byArticleId;
+export const isLoadingComments = (state) => state.comments.isLoadingComments;
+export const createCommentIsPending = (state) => state.comments.createCommentIsPending;
+
+export default commentsSlice.reducer;
